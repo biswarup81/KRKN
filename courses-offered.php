466dbba1a8d477643c1910f4c6c2043e1b330ac6
Biswarup Ghoshal <biswarup.ghoshal@gmail.com>
2018-06-01 16:29:33 +0530

    Added footer section.

--- courses-offered.php
+++ courses-offered.php
@@ -145,39 +145,6 @@ include_once './inc/header.php'; ?>
 <!-- ================================================== -->
 
 <div class="clearfix"></div>
-<footer class="footer clearfix">
-  <div class="container clearfix padding-off">
-    <div class="footer-col col-lg-4 pull-left ">
-      <h4 class="footer-title text-gradient-span"><span>Contact Information</span></h4>
-      <div class="footer-entry clearfix">
-        <div class="addre-row"><i class="fa fa-phone" aria-hidden="true"></i> +91 (03453) 273372</div>
-        <div class="addre-row"><i class="fa fa-envelope" aria-hidden="true"></i>Village & Post - Kandra,<br>
-          P.S-Ketugram,<br>
-          Dist - Burdwan, WB.<br>
-          Pincode -713129 </div>
-      </div>
-    </div>
-    <div class="footer-col col-lg-4 pull-left ">
-      <h4 class="footer-title text-gradient-span"><span>Locate Us </span></h4>
-      <div class="footer-entry clearfix"> </div>
-    </div>
-    <div class="footer-col col-lg-4 pull-left ">
-      <h4 class="footer-title text-gradient-span"><span>Useful Links</span></h4>
-      <div class="footer-entry clearfix"> </div>
-    </div>
-  </div>
-  <div class="copyright col-lg-12 clearfix">
-    <div class="container clearfix padding-off">
-      <div class="col-lg-6">&copy; 2011 <strong>Kandra Radhakanta Kundu Mahavidyalaya</strong>. All Rights Reserved.</div>
-      <div class=" pull-right">Conceptualised by : <strong>PG Infoservices</strong></div>
-    </div>
-  </div>
-</footer>
-<script src="assets/js/jquery.js"></script> 
-<script src="dist/js/bootstrap.min.js"></script> 
-<script src="assets/js/holder.js"></script> 
-<script src="dist/js/slick.js"></script> 
-<script src="dist/js/colorbox.js"></script> 
-<script src="dist/js/custom-script.js"></script>
+<?php include_once './inc/footer.php';?>
 </body>
 </html>
